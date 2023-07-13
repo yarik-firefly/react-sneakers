79d97cec5d961f6f5b8d3ce8ebfee2101dfffcd9
yarik-firefly <snowboardvgostahu@gmail.com>
2023-07-13 13:35:41 +0300

    Updates

--- src/pages/context.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-// Создаём отдельный компонент в котором создаём Контекст и экспортируем его
-const AppContext = React.createContext<any>({});
-
-export default AppContext;
